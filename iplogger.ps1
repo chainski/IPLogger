161e20db93d3ee6e277e4bca708d8354924fc2d4
chainskimo <96607632+chainski@users.noreply.github.com>
2026-02-02 12:31:18 -0500

    Add files via upload

--- iplogger.ps1
+++ iplogger.ps1
@@ -3,9 +3,9 @@ $ProgressPreference = 'SilentlyContinue'
 $ErrorActionPreference = "SilentlyContinue"
 $LINK = " " # ADD YOUR IPLOGGER URL HERE
 function IPLogger {
-    $userAgents = (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microlinkhq/top-user-agents/v2.1.57/src/index.json").Content | ConvertFrom-Json 
+    $userAgents = (Invoke-WebRequest -UseBasicParsing -Uri "https://raw.githubusercontent.com/microlinkhq/top-user-agents/v2.1.57/src/index.json").Content | ConvertFrom-Json 
     $userAgent = $userAgents | Get-Random
-    $response = Invoke-WebRequest -Uri $LINK -UserAgent $userAgent;return $response
+    $response = Invoke-WebRequest -UseBasicParsing -Uri $LINK -UserAgent $userAgent;return $response
 	Write-Host -ForegroundColor Green "[+] IP logged successfully ! You may now check the results on iplogger.org via your tracking code"  
 }
 IPLogger
